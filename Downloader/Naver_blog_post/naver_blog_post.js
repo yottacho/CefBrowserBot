afb9bf066167f247ef16afa0257df6e61fec4776
Yotta <seearomi@outlook.com>
2021-05-22 17:02:59 +0900

    updated

--- Downloader/Naver_blog_post/naver_blog_post.js
+++ Downloader/Naver_blog_post/naver_blog_post.js
@@ -9,6 +9,17 @@ window.onkeydown = function (event) {
     }
 }
 
+async function auto_download_images() {
+    // 자동다운로드가 ON이면 자동다운로드 수행...
+    var autoDownloadFlag = await $Downloader$.getAutoDownloadFlag();
+    console.log("자동 다운로드: " + autoDownloadFlag);
+
+    if (autoDownloadFlag != true)
+        return;
+
+    download_images();
+}
+
 /* ***************************************************************
  * 다운로드 처리
  * *************************************************************** */
@@ -18,6 +29,11 @@ async function download_images() {
 
     var msg_id = document.getElementById("$Downloader$_Message");
     msg_id.innerHTML = "<font color=\"#99ccff\">요청 상태: </font><strong style=\"color: #66ffff;\">다운로드 요청중 ...</strong>";
+
+    if (img_list.length == 0) {
+        msg_id.innerHTML = "<font color=\"#99ccff\">요청 상태: </font><strong style=\"color: #66ffff;\">이미지 없음</strong>";
+        return;
+    }
 
     // 다운로드 요청
     var result = $Downloader$.downloadImages(title, img_list, location.href, navigator.userAgent);
@@ -45,6 +61,31 @@ function get_images_list() {
     get_images_list_class(document.getElementsByClassName("se_mediaImage"), images_url_list);       // old se editor
     get_images_list_class(document.getElementsByClassName("_photoImage"), images_url_list);         // old blog
 
+    if (images_url_list.length == 0) {
+        // _postView 아래의 img 태그
+        var post_view = document.getElementsByClassName("_postView");
+        if (post_view.length > 0) {
+            var post_imgs = post_view[0].getElementsByTagName("img");
+            for (i = 0; i < post_imgs.length; i++) {
+                if (post_imgs[i].hasAttribute("largesrc")) {
+                    var u = new URL(post_imgs[i].src, location.href);
+
+                    if (u.hostname == "postfiles.pstatic.net" || u.hostname == "blogthumb.pstatic.net" ||
+                        u.hostname == "mblogthumb-phinf.pstatic.net") {
+                        // blogfiles.naver.net => blogfiles.pstatic.net
+                        u.hostname = "blogfiles.pstatic.net";
+                    }
+
+                    // 이미지 주소(파라마터 없음)
+                    var img_url = u.origin + u.pathname;
+
+                    console.log(img_url);
+                    images_url_list.push(img_url);
+                }
+            }
+        }
+    }
+
     console.log("Image list > " + images_url_list.length);
     return images_url_list;
 }
@@ -55,7 +96,8 @@ function get_images_list_class(clsName, images_url_list) {
         if (clsName[i].tagName == "IMG") {
             var u = new URL(clsName[i].src, location.href);
 
-            if (u.hostname == "postfiles.pstatic.net" || u.hostname == "blogthumb.pstatic.net") {
+            if (u.hostname == "postfiles.pstatic.net" || u.hostname == "blogthumb.pstatic.net" ||
+                u.hostname == "mblogthumb-phinf.pstatic.net") {
                 // blogfiles.naver.net => blogfiles.pstatic.net
                 u.hostname = "blogfiles.pstatic.net";
             }
@@ -161,6 +203,10 @@ function get_title() {
         moreLink[0].click();
     }
 
+    show_info_bar();
+}
+
+function show_info_bar() {
     // add div
     var html = "";
     var title = get_title();
@@ -170,7 +216,7 @@ function get_title() {
     div.id = "$Downloader$_Ident";
 
     div.style.position = "fixed";
-    div.style.top = (window.innerHeight - 35) + "px";
+    div.style.top = (window.innerHeight - 75) + "px";
     div.style.left = "30px";
     div.style.width = "95%";
     div.style.height = "30px";
@@ -184,20 +230,18 @@ function get_title() {
     div.style.color = "#f0f0f0";
 
     html += "<table style=\"border: 0; padding: 0; border-spacing: 0; width: 100%; \"><tr>";
-    html += "<td><a href=\"javascript:download_images()\" style=\"color: #f0f0f0; \">";
-    html += "분류: <strong style=\"color:#ffff80;\">" + title[0] + "</strong>, ";
-    html += "제목: <strong style=\"color:#ffff80;\">" + title[1] + "</strong> </a>";
-    html += "<font color=\"#bbbbbb\">(이미지: " + img_list.length + "개)</font></td>";
-    html += "<td><span id=\"$Downloader$_Message\">&nbsp;</span></td>";
+    html += "<td nowrap><a href=\"javascript:download_images()\" style=\"color: #f0f0f0; font-size: 10pt;\">";
+    html += "분류: <strong style=\"color:#ffff80; font-size: 10pt;\">" + title[0] + "</strong>, ";
+    html += "제목: <strong style=\"color:#ffff80; font-size: 10pt;\">" + title[1] + "</strong> </a>";
+    html += "<font color=\"#bbbbbb\" style=\"font-size: 10pt;\">(이미지: " + img_list.length + "개)</font></td>";
+    html += "<td width=\"300\" nowrap><span id=\"$Downloader$_Message\" style=\"font-size: 10pt;\">&nbsp;</span></td>";
     html += "</tr></table>";
-
-    //html += "<span><a href=\"javascript:download_images()\" style=\"color: #ffffff; \">";
-    //html += "분류: " + title[0] + ", 제목: " + title[1];
-    //html += " (이미지:" + img_list.length + "개)";
-    //html += "</a></span> &nbsp; &nbsp;<span id=\"$Downloader$_Message\">&nbsp;</span>";
 
     div.innerHTML  = html;
 
     document.body.appendChild(div);
     // div add end
+
+    // 이미지 다운로드 처리
+    setTimeout('auto_download_images()', 1000);
 }
